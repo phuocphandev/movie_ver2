c852123f7a69925a07ad1eab8efd9760bdd617fa
tgkh175 <trangiangkhanh04@gmail.com>
2023-10-24 14:36:25 +0700

    test

--- src/components/template/Introduction/Introduction.jsx
+++ src/components/template/Introduction/Introduction.jsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
-import { useSprings, animated, to as interpolate, UnknownProps } from "@react-spring/web";
+import { useSprings, animated, to as interpolate} from "@react-spring/web";
 
 import styles from "./styles.module.css";
 import { useDrag } from "@use-gesture/react";
-import { DrawerPanelProps } from "antd/es/drawer/DrawerPanel";
 
 const cards = [
   "/image/introPoster/morbius.jpg",
@@ -15,16 +14,18 @@ const cards = [
 ];
 
 // These two are just helpers, they curate spring data, values that are later being interpolated into css
-const to = (i: number) => ({
+const to = (i) => ({
   x: 0,
   y: i * -4,
   scale: 1,
   rot: -10 + Math.random() * 20,
   delay: i * 100,
 });
-const from = (_i: number) => ({ x: 0, rot: 0, scale: 1.5, y: -1000 });
+
+const from = () => ({ x: 0, rot: 0, scale: 1.5, y: -1000 });
+
 // This is being used down there in the view, it interpolates rotation and scale into a css transform
-const trans = (r: number, s: number) =>
+const trans = (r, s) =>
   `perspective(1500px) rotateX(30deg) rotateY(${
     r / 10
   }deg) rotateZ(${r}deg) scale(${s})`;
@@ -33,7 +34,7 @@ export const Introduction = () => {
   const [gone] = useState(() => new Set()); // The set flags all the cards that are flicked out
   const [props, api] = useSprings(cards.length, (i) => ({
     ...to(i),
-    from: from(i),
+    from: from(),
   })); // Create a bunch of springs using the helpers above
   // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
   const bind = useDrag(
