eacad8d2da3015a801a35c47ea1be864fe477fcd
tgkh175 <trangiangkhanh04@gmail.com>
2023-10-24 14:31:48 +0700

    fix Intro

--- src/components/template/Introduction/Introduction.tsx
+++ src/components/template/Introduction/Introduction.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
-import { useSprings, animated, to as interpolate } from "@react-spring/web";
+import { useSprings, animated, to as interpolate, UnknownProps } from "@react-spring/web";
 
 import styles from "./styles.module.css";
 import { useDrag } from "@use-gesture/react";
+import { DrawerPanelProps } from "antd/es/drawer/DrawerPanel";
 
 const cards = [
   "/image/introPoster/morbius.jpg",
@@ -35,59 +36,65 @@ export const Introduction = () => {
     from: from(i),
   })); // Create a bunch of springs using the helpers above
   // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
- const bind = useDrag(({ args: [index], down, movement: [mx], direction: [xDir], velocity }) => {
-  const trigger = velocity[0] > 0.2 ;
-  const dir = xDir < 0 ? -1 : 1;
+  const bind = useDrag(
+    ({ args: [index], down, movement: [mx], direction: [xDir], velocity }) => {
+      const trigger = velocity[0] > 0.2;
+      const dir = xDir < 0 ? -1 : 1;
 
-  if (!down && trigger) {
-    gone.add(index);
-  }
+      if (!down && trigger) {
+        gone.add(index);
+      }
 
-  api.start((i) => {
-    if (index !== i) return;
-    // console.log("idx: ", index, "-------i: ", i)
-   
-    const isGone = gone.has(index);
-    const x = isGone 
-    ? ((200 + window.innerWidth) * dir) 
-    : ((down)? mx : 0);
+      api.start((i) => {
+        if (index !== i) return;
+        // console.log("idx: ", index, "-------i: ", i)
 
-    const rot = mx / 100 + (isGone ? dir * 10 * velocity[0] : 0);
-    const scale = down ? 1.1 : 1;
+        const isGone = gone.has(index);
+        const x = isGone ? (200 + window.innerWidth) * dir : down ? mx : 0;
 
-    return {
-      x,
-      rot,
-      scale,
-      delay: undefined,
-      config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 },
-    };
-  });
+        const rot = mx / 100 + (isGone ? dir * 10 * velocity[0] : 0);
+        const scale = down ? 1.1 : 1;
 
-  
-  if (!down && gone.size === cards.length) {
-    setTimeout(() => {
-      gone.clear();
-      api.start((i) => to(i));
-    }, 600);
-  }
-});
+        return {
+          x,
+          rot,
+          scale,
+          delay: undefined,
+          config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 },
+        };
+      });
+
+      if (!down && gone.size === cards.length) {
+        setTimeout(() => {
+          gone.clear();
+          api.start((i) => to(i));
+        }, 600);
+      }
+    }
+  );
   // Now we're just mapping the animated values to our view, that's it. Btw, this component only renders once. :-)
   return (
     <div className="relative h-[35vh] md:h-[60vh]  w-screen flex justify-center items-center mb-[10vh] ">
-      {props.map(({ x, y, rot, scale }, i) => (
-        <animated.div className={`${styles.deck} w-[90%] md:w-[45%] `} key={i} style={{ x, y }}>
-          {/* This is the card itself, we're binding our gesture to it (and inject its index so we know which is which) */}
+      {props.map(({ x, y, rot, scale }, i) => {
+        console.log("bind: ", bind(i))
+        return (
           <animated.div
-            {...bind(i)}
-            style={{
-              transform: interpolate([rot, scale], trans),
-              backgroundImage: `url(${cards[i]})`,
-              width:"",
-            }}
-          />
-        </animated.div>
-      ))}
+            className={`${styles.deck} w-[90%] md:w-[45%] `}
+            key={i}
+            style={{ x, y }}
+          >
+            {/* This is the card itself, we're binding our gesture to it (and inject its index so we know which is which) */}
+            <animated.div
+              {...bind(i)}
+              style={{
+                transform: interpolate([rot, scale], trans),
+                backgroundImage: `url(${cards[i]})`,
+                width: "",
+              }}
+            />
+          </animated.div>
+        );
+      })}
     </div>
   );
 };
